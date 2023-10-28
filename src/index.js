908c80efa6720c21b4a9b8d0283cc1d89fab74d7
Ruth <ruro122020@gmail.com>
2023-10-28 18:17:29 -0400

    create NavBar component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 //step 1. Import react-router functions
 import { createBrowserRouter, RouterProvider,  } from "react-router-dom";
 import Home from "./pages/Home";
-
+import About from './pages/About'
+import Login from './pages/Login'
 const router = createBrowserRouter([
     {
         path:'/',
         element: <Home />
+    },
+    {
+        path:'/about',
+        element:<About />
+    },
+    {
+        path:'/login',
+        element: <Login />
     }
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,10 @@
+import NavBar from "../components/NavBar";
 function Login() {
     return (
       <>
         <header>
           {/* Save space for NavBar */}
+          <NavBar />
         </header>
         <main>
           <h1>Login</h1>
